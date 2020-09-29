f1fa21e2c8e6637bb094ad19ecf61dac82f19583
Sachet Adhikari <sachet.adhi@gmail.com>
2020-09-29 22:00:58 +0545

    init and config error fix

---
+++ appp/__init__.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from authlib.integrations.flask_client import OAuth
+from appp.config import Config
+
+
+app=Flask(__name__)
+app.config.from_object(Config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db= SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt= Bcrypt(app)
+oauth= OAuth(app)
+login_manager= LoginManager(app)
+login_manager.login_view= 'login'
+login_manager.login_message_category = 'info'
+
+mail = Mail(app)
+
+
+G = oauth.register(
+    name='google',
+    client_id=os.environ.get('CLIENT_ID'), #enter your client id
+    client_secret=os.environ.get('CLIENT_SECRET'), #enter your client secret key
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
+    client_kwargs={'scope': 'openid email profile'},
+)
+
+from appp import routes
---
+++ appp/config.py
@@ -0,0 +1,8 @@
+class Config():
+    SECRET_KEY='secretKey12345' #for development purpose only
+    SQLALCHEMY_DATABASE_URI='sqlite:///site.db' #for development purpose only
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME ='example@gmail.com' #enter gmail id
+    MAIL_PASSWORD ='yourPassword' #enter gmail password
