7b89db15737cd72710137af630895739ab7f9c50
Sachet Adhikari <66741255+SachetAdhikari@users.noreply.github.com>
2020-09-29 20:41:14 +0545

    Delete config.py

--- appp/config.py
+++
@@ -1,9 +0,0 @@
-from appp import app
-class Config():
-    app.config['SECRET_KEY']= '138a79505fa3993f6a50dce42f55bd7de4b7ecb1'
-    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = '#' #os.environ.get('EMAIL_USER')
-    app.config['MAIL_PASSWORD'] = '#' #os.environ.get('EMAIL_PASS')
